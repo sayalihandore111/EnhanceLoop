17c0a143d997056c196d7d02faf0932e61b1435d
sayalihandore111 <sayalihandore111@gmail.com>
2021-08-31 20:52:20 +0530

    Add project files.

---
+++ EnhanceLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EnhanceLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print array:");
+
+            // creating an array
+            int[] a_array = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            // foreach loop begin
+            // it will run till the
+            // last element of the array
+            foreach (int items in a_array)
+            {
+                Console.WriteLine(items);
+            }
+        }
+    }
+}
